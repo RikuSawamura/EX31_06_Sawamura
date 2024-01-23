99c966e2282f048bed27b96d6144d1f03c0b8caf
GAME\232e005 <riku.sawamura.0718@gmail.com>
2024-01-23 17:14:17 +0900

    数値変更

--- Ex31_hint/Program.cs
+++ Ex31_hint/Program.cs
@@ -7,15 +7,15 @@ namespace Ex31_hint
     {
         static void Main(string[] args)
         {
-            Human human1 = new Human("しむら人", 500, 2, "ヒト");
+            Human human1 = new Human("しむら人", 500, 15, "ヒト");
             Human human2 = new Human("ささっき", 1000, 25, "ヒト");
             Tank tank1 = new Tank("Tiger", 500000, 250);
             tank1.bullet = 10;
             Tank tank2 = new Tank("がんタンク", 10000, 125);
             tank2.bullet = 5;
             Caw caw = new Caw("暴れ牛", 3000, 100, "ウシ");
-            Cat cat = new Cat("野良猫", 1500, 75, "ネコ");
-            Hornet hornet = new Hornet("蜂", 200, 20, "ハチ");
+            Cat cat = new Cat("野良猫", 300, 75, "ネコ");
+            Hornet hornet = new Hornet("蜂", 100, 20, "ハチ");
             Character[] characters = { human1, human2, tank1, tank2, caw, cat, hornet };
             Random rand = new Random();
             int r = rand.Next(characters.Length);
